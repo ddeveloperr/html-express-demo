29ea4587c12ffd6a6f24cce17a658b70c05a8434
KemalC <ddeveloperr@gmail.com>
2017-07-10 22:45:36 +0000

    Add basic html with render method

--- html-express-demo/app.js
+++ html-express-demo/app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(rer, res){
-   res.send("Welcome to the home page!<br><h1>Node.js Render HTML now!</h1><p>Enjoy now</p>"); 
+   //res.send("Welcome to the home page!<br><h1>Node.js Render HTML now!</h1><p>Enjoy now</p>"); 
+   res.render("home.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
