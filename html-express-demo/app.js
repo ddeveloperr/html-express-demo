fb81ac2bf4f408befc38918d5e97591575103f18
KemalC <ddeveloperr@gmail.com>
2017-07-10 22:34:24 +0000

    Render html without render method.

--- html-express-demo/app.js
+++ html-express-demo/app.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(rer, res){
-   res.send("Welcome to the home page!"); 
+   res.send("Welcome to the home page!<br><h1>Node.js Render HTML now!</h1><p>Enjoy now</p>"); 
 });
 
-app.listening(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening...");
 });
 
