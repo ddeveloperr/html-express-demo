531557b689f1fd7cb3e93862e9406e0e4087c05a
KemalC <ddeveloperr@gmail.com>
2017-07-11 19:48:04 +0000

    Add love.ejs, update app.js with thingVar

--- html-express-demo/app.js
+++ html-express-demo/app.js
@@ -6,6 +6,13 @@ app.get("/", function(rer, res){
    res.render("home.ejs");
 });
 
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    // res.send("You fell in love with " + thing);
+    res.render("love.ejs", {thingVar: thing});
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening...");
 });
