551a09c344a128e197f07e760d6e0ef8a2468603
KemalC <ddeveloperr@gmail.com>
2017-07-12 19:48:17 +0000

    Use if statements and loops in ejs

--- 4-html-express-demo/app.js
+++ 4-html-express-demo/app.js
@@ -12,7 +12,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
-
+app.get("/posts", function(req, res){
+    var posts = [
+        {title:"Post 1", author:"Author 1"},
+        {title:"Post 2", author:"Author 2"},
+        {title:"Post 3", author:"Author 3"},
+    ];
+    
+    res.render("posts.ejs", {posts: posts});
+    
+});
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening...");
 });
